8f1fffe7162be288f3cd2feb016f889d34822388
Faizan Amjad <faizan.amjad@now.net.pk>
2017-12-15 18:20:06 +0500

    add property ISOCODE in language table

--- CMSCarousel.Entities/thac_language.cs
+++ CMSCarousel.Entities/thac_language.cs
@@ -22,6 +22,7 @@ namespace CMSCarousel.Entities
     
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ISOCode { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<thac_languageoption> thac_languageoption { get; set; }
